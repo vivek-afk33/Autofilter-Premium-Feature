e38d951041723f8e9cba55bfd945aa98b7bbf034
vivek-afk33 <khatorvivek92@gmail.com>
2026-07-29 21:48:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -140,8 +140,7 @@ MAX_B_TN = environ.get("MAX_B_TN", "5") # Maximum number of buttons in a row (de
 PORT = int(environ.get("PORT", "8080"))  # Port for the web server (default: 8080)
 MSG_ALRT = environ.get('MSG_ALRT', 'Share & Support Us ♥️') # Alert message for users
 DELETE_TIME = int(environ.get("DELETE_TIME", "300"))  #  deletion time in seconds (default: 5 minutes). Adjust as per your needs.
-CUSTOM_FILE_CAPTION = environ.get('CUSTOM_FILE_CAPTION', '<b>{file_name}</b>\n\n⚜️ <b>Powered By : <a href="https://t.me/Viku_Flix_Backup">[ Viku Flix ]</a></b>')
-
+CUSTOM_FILE_CAPTION = environ.get('CUSTOM_FILE_CAPTION', f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION) # Custom caption for batch files
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", f"{script.IMDB_TEMPLATE_TXT}")     # Custom IMDB template 
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None) # Maximum number of elements in a list (default: None, no limit)
